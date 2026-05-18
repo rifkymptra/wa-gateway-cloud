754dc60b2b6fc790103f4358470c19f053b56ad3
MyPC PRO L7V <rifky.mputra8@gmail.com>
2026-05-18 13:11:36 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const axios = require('axios');
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('WhatsApp Bot Ready!');
+});
+
+client.on('message', async message => {
+
+    console.log('Pesan masuk:', message.body);
+
+    let mediaData = null;
+
+    if (message.hasMedia) {
+
+        const media = await message.downloadMedia();
+
+        mediaData = {
+            mimetype: media.mimetype,
+            data: media.data,
+            filename: media.filename
+        };
+
+        console.log('Media terdeteksi!');
+    }
+
+    try {
+
+        await axios.post('http://127.0.0.1:8000/wa-webhook', {
+            nomor: message.from,
+            pesan: message.body,
+            media: mediaData
+        });
+
+        console.log('Berhasil kirim ke Laravel');
+
+    } catch (error) {
+
+        console.log('Gagal kirim:', error.message);
+
+    }
+
+});
+
+client.initialize();
